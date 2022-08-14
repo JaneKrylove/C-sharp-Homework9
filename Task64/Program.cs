9da45a1734ae914b8d23299b1fd5a696a9f2048a
Jane <spring1981@mail.ru>
2022-08-14 09:48:31 +0300

    task64 updated

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+
+int CountDigits(int number)
+{
+    if (number / 10 != 0) return 1 + CountDigits(number / 10);
+
+    else return 1;
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number of digits in number: " + CountDigits(num));
